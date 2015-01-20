1862a922ca1dfe588a3c5919eddbd5dd1e7bc66f
Bohdan Korniyenko <bohdan.korniyenko@gmail.com>
2015-01-20 12:39:36 +0200

    change test

--- src/master_fsm.erl
+++ src/master_fsm.erl
@@ -1,15 +1,15 @@
 -module(master_fsm).
 -behaviour(gen_fsm).
 
--export([test/1,run_on_worker/6]).
+-export([test/0,run_on_worker/6]).
 -export([init/1,handle_info/3,idle/2]).
 
 -record(state, {client}).
 
 -define(THRESHOLD,5).
 
-test(Nodes) ->
-  lager:start(),
+test() ->
+  Nodes = gen_server:call({cluster_manager, 'gc@104.131.46.157'}, get_nodes),
   {ok, Pid} = gen_fsm:start_link(?MODULE, {}, []),
   ok = gen_fsm:send_event(Pid, {run, 20, "ATGTGACACAGATCACTGCGGCCTTGACCTCCCAGGCTCCAGGTGGTTCTT","21","/home/drc/bwt_files/human_g1k_v37_decoy.fasta", Nodes}).
 
@@ -17,15 +17,16 @@ init(_Args) ->
   {ok, idle, #state{}}.
 
 handle_info(Matches, busy, State) when is_list(Matches) ->
-io:format("master got matches: ~n~w~n", [Matches]),
+io:format("master got matches: ~n~p~n", [Matches]),
   {next_state, busy, State}.
 
 idle({run, N,Seq,Ref_seq_name,File,Nodes}, State) -> 
   ReturnPid = self(),
   lists:foldl(fun(NodeName,J) ->
-    spawn_link(NodeName, ?MODULE, run_on_worker, [N,Seq,Ref_seq_name,File,ReturnPid,J]),
+    Pid = spawn_link(NodeName, ?MODULE, run_on_worker, [N,Seq,Ref_seq_name,File,ReturnPid,J]),
+    io:format("spawned ~p~n", [Pid]),
     J+1
-  end, 3, Nodes),
+  end, 0, Nodes),
   {next_state, busy, State}.
 
 run_on_worker(N,Seq,Ref_seq_name,File,ReturnPid,J) ->
