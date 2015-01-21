a47d4fcc455ad82be97409090243a18fcd005cf2
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2015-01-21 16:07:54 +0200

    get_schedule modified

--- src/schedule.erl
+++ src/schedule.erl
@@ -4,26 +4,28 @@
 %
 
 -module(schedule).
--export([get_schedule/3]).
+-export([get_schedule/3, get_workload_and_index/3]).
 
 -define(QSEQ_SIZE,100).
 
+
+
 % returns the list of lists that contain tuples (start_point, end_point)
 % each list is a schedule for a compute node
-get_schedule(Index_file, Chunk_size, Nodes_nbr) ->
+get_workload_and_index(Index_file, Chunk_size, Nodes_nbr) ->
 	case file:open(Index_file,read) of
 		{ok,Dev} ->
-			{Workload,[{Pos,Len,_}|Partitions]} = get_average_workload_and_parts(0, [], Dev, Chunk_size, Nodes_nbr),
+			{Workload,Partitions} = get_average_workload_and_parts(0, [], Dev, Chunk_size, Nodes_nbr),
 			file:close(Dev),
 			io:format("Average workload=~.2g~n",[Workload]),
-			distribute_parts([], [], 0, Workload, Pos,Len,Partitions, Chunk_size);
+			{Workload,Partitions}
 			
 		{error,Reason} ->
 			io:format("index file: '~s' cannot be opened~n~p~n",[Index_file,Reason]),
 			error
 	end.
 
-
+			
 
 get_average_workload_and_parts(Sum, Acc, Dev, Chunk_size, Nodes_nbr) ->
 	case file:read_line(Dev) of
@@ -47,6 +49,9 @@ get_part([{P,Len,_}|Parts_sorted],Pos) when Pos > P+Len -> get_part(sorted,Parts
 get_part(_,_) -> no_name_found.
 
 
+
+get_schedule(Workload,[{Pos,Len,_}|Partitions],Chunk_size) ->
+	distribute_parts([], [], 0, Workload, Pos,Len,Partitions, Chunk_size).
 
 distribute_parts(Acc, Ls, Curr_workload, Avg_workload, 
 	Curr_pos, Rest, Partitions, Chunk_size) when Curr_workload >= Avg_workload ->
