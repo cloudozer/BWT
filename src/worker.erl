63e64c64e0421a5c5ae9ef0a6ded0b0d12341ebe
Bohdan Korniyenko <bohdan.korniyenko@gmail.com>
2015-01-21 11:33:15 +0200

    rename registered process

--- src/worker.erl
+++ src/worker.erl
@@ -9,7 +9,7 @@ seq_file_reader/1, seq_file_reader_loop/1, worker_loop/0]).
 -record(state, {current_worker, tasks_queue, master_pid, seq, ref_file_abs}).
 
 start_link() ->
-  gen_fsm:start_link({local, ?MODULE}, ?MODULE, {}, []).
+  gen_fsm:start_link({local, worker_bwt}, ?MODULE, {}, []).
 
 run(Pid, Args) ->
   gen_fsm:sync_send_event(Pid, {run, Args}).
