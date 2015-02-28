c22e13df5144e2ffc0b9c05d5bc7cc25f83a89e1
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2015-02-28 06:52:15 +0200

    An FM-index added to bwt module

--- src/bwt.erl
+++ src/bwt.erl
@@ -4,7 +4,14 @@
 %
 
 -module(bwt).
--export([bwt/1,get_suffs/1,bwt_sa/1]).
+-export([bwt/1,
+		get_suffs/1,
+		fm/1,
+		rank_all/1,
+		sa/1,
+		test/1]).
+
+-record(fm,{f,l,d,a,c,g,t,sa}).
 
 
 
@@ -27,12 +34,43 @@ get_all_permutations(Acc,T,K,N) ->
 	get_all_permutations([NewT|Acc],NewT,K-1,N).
 
 
-bwt_sa(X) ->
+fm(X) ->
 	Ls = lists:sort(get_suffs(X)),
-	[ case N of
-		0 -> $$;
-		J -> lists:nth(J,X)
-	  end || {_,N} <- Ls].
+	%io:format("Sufs:~p~n",[Ls]),
+	Index = lists:reverse( lists:foldl(fun({[H|_],N},Acc)->
+		case N of
+			0 -> [#fm{f=H, l=$$, sa=0}|Acc];
+			J -> [#fm{f=H, l=lists:nth(J,X), sa=J}|Acc]
+		end
+										end,[],Ls)),
+	rank_all(Index).
+	
+
+rank_all(FM) ->
+	rank_all(FM,[],0,0,0,0,0).
+
+rank_all([Rec=#fm{l=L}|FM],Acc,Dn,An,Cn,Gn,Tn) ->
+	case L of
+		$A -> rank_all(FM,[Rec#fm{d=Dn,a=An+1,c=Cn,g=Gn,t=Tn}|Acc],Dn,An+1,Cn,Gn,Tn);
+		$C -> rank_all(FM,[Rec#fm{d=Dn,a=An,c=Cn+1,g=Gn,t=Tn}|Acc],Dn,An,Cn+1,Gn,Tn);
+		$G -> rank_all(FM,[Rec#fm{d=Dn,a=An,c=Cn,g=Gn+1,t=Tn}|Acc],Dn,An,Cn,Gn+1,Tn);
+		$T -> rank_all(FM,[Rec#fm{d=Dn,a=An,c=Cn,g=Gn,t=Tn+1}|Acc],Dn,An,Cn,Gn,Tn+1);
+		$$ -> rank_all(FM,[Rec#fm{d=Dn+1,a=An,c=Cn,g=Gn,t=Tn}|Acc],Dn+1,An,Cn,Gn,Tn)
+	end;
+rank_all([],Acc,_,_,_,_,_) -> lists:reverse(Acc).
+	
+
+test(X) ->
+	FM = fm(X),
+	rank_all(FM).
+
+
+
+sa(X) ->
+	Ls = lists:sort(get_suffs(X)),
+	[ N || {_,N} <- Ls].
+
+
 
 get_suffs(X) ->
 	get_suffs("",0,X++"$").
