c59e369eb8a0b9c905d2e392ce4a47ed38ac5406
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2015-03-14 09:49:10 +0200

    a fast seed finding using BWT developed

--- src/bwt.erl
+++ src/bwt.erl
@@ -8,10 +8,39 @@
 		get_suffs/1,
 		fm/1,
 		sa/1,
-		find_match/2
+		find_match/2,
+		make_index/0,
+		test/0
 		]).
 
+-define(TRSH,0.8).
+
+
 %-record(fm,{f,l,d,a,c,g,t,sa}).
+
+test() ->
+	%"GGCTCTGTTAGAGTAGATAGCTAGCTAGACATGAACAGGAGGGGGAGCTCCTGGAAAAGG",
+	%"GAAAGTCTGTGAAGGCTCACCTGGAGGGACCACCAAAAATGCACATATTAGTAGCATCTC",
+	%"TAGTGCTGGAGTGGATGGGCACTTGTCAATTGTGGTTAGGAGGGAGAAGAGGTACCTACG",
+	%"CAGAAACACCCTAGAACTTCTCTTAAGGTGCCCCAATCGGATGGGCGCGGTGGCTCACGC",
+	Qseq = "CTGTAATCCCAGCACTTTGGGAGGCCGAGGCGGGTGGATCATGAGGTCAGGAGATCGAGA",
+	%"CCATCCTGGCTAACAAGGTGAAACCCCGTCTCTACTAAAAATACAAAAAATTAGCCGGGC"
+	FM = get_index(),
+	{Subseq,Pos} = get_subseq(Qseq),
+	[ X+Pos || X<-find_match(Subseq,FM) ].
+
+	
+make_index() ->
+	File = "../bwt_files/human_g1k_v37_decoy.fasta",
+	{Pos,Len} = msw:get_reference_position("21",File),
+
+	Bin = term_to_binary(fm(msw:get_chunk(File,Pos+Len div 2,Len div 10))),
+	file:write_file("../bwt_files/fm_index",Bin).
+
+
+get_index() ->
+	{ok,Bin} = file:read_file("../bwt_files/fm_index"),
+	binary_to_term(Bin).
 
 
 
@@ -36,25 +65,35 @@ get_all_permutations(Acc,T,K,N) ->
 
 %% returns an FM index for a given reference sequence
 fm(X) ->
+	_ = statistics(runtime),
 	Ls = lists:sort(get_suffs(X)),
+	{_,T2} = statistics(runtime),
+	io:format("Suffix array generation took: ~psec~n",[T2/1000]),
 	%io:format("Sufs:~p~n",[Ls]),
-	{FM, Dq, Aq, Cq, Gq, Tq} = fm(X,Ls,[],1,{[],[],[],[],[]}),
+	{FM, Dq, Aq, Cq, Gq, Tq} = fm(X,Ls,[],1,[],[],[],[],[]),
+	{_,T3} = statistics(runtime),
+	io:format("Building the queues took ~p sec~n",[T3/1000]),
+
 	list_to_tuple(add_indices(FM,[],Dq,Aq,Cq,Gq,Tq)).
+	%{_,T4} = statistics(runtime),
+	%io:format("Building the index took ~p sec~n",[T4/1000]).
 
 
-fm(X,[{[S|_],N}|Ls], Acc, K, {Dq,Aq,Cq,Gq,Tq}) ->
-	case N of
-		0 -> Acc1 = [{S,$$,0}|Acc];
-		J -> Acc1 = [{S,lists:nth(J,X),J}|Acc]
-	end,
+
+fm(X,[{[S|_],N,P}|Ls], Acc, K, Dq,Aq,Cq,Gq,Tq) ->
+	%case N of
+		%%%%%%%%%%%  {F,L,SA}
+	%	0 -> Acc1 = [{S,$$,P}|Acc];
+	%	J -> Acc1 = [{S,lists:nth(J,X),J}|Acc]
+	%end,
 	case S of
-		$A -> fm(X,Ls,Acc1,K+1,{Dq,[K|Aq],Cq,Gq,Tq});
-		$C -> fm(X,Ls,Acc1,K+1,{Dq,Aq,[K|Cq],Gq,Tq});
-		$G -> fm(X,Ls,Acc1,K+1,{Dq,Aq,Cq,[K|Gq],Tq});
-		$T -> fm(X,Ls,Acc1,K+1,{Dq,Aq,Cq,Gq,[K|Tq]});
-		$$ -> fm(X,Ls,Acc1,K+1,{[K|Dq],Aq,Cq,Gq,Tq})
+		$A -> fm(X,Ls,[{S,P,N}|Acc],K+1,Dq,[K|Aq],Cq,Gq,Tq);
+		$C -> fm(X,Ls,[{S,P,N}|Acc],K+1,Dq,Aq,[K|Cq],Gq,Tq);
+		$G -> fm(X,Ls,[{S,P,N}|Acc],K+1,Dq,Aq,Cq,[K|Gq],Tq);
+		$T -> fm(X,Ls,[{S,P,N}|Acc],K+1,Dq,Aq,Cq,Gq,[K|Tq]);
+		$$ -> fm(X,Ls,[{S,P,N}|Acc],K+1,[K|Dq],Aq,Cq,Gq,Tq)
 	end;
-fm(_,[], Acc, _, {Dq,Aq,Cq,Gq,Tq}) -> 
+fm(_,[], Acc, _, Dq,Aq,Cq,Gq,Tq) -> 
 	{lists:reverse(Acc),
 	lists:reverse(Dq),
 	lists:reverse(Aq),
@@ -89,35 +128,35 @@ add_indices([],Acc,[],[],[],[],[]) -> lists:reverse(Acc).
 
 sa(X) ->
 	Ls = lists:sort(get_suffs(X)),
-	[ N || {_,N} <- Ls].
+	[ N || {_,N,_} <- Ls].
 
 
 
 get_suffs(X) ->
-	get_suffs("",0,X++"$").
+	get_suffs([],0,X++"$",$$).
 
-get_suffs(Acc, N, [H|X]) ->
-	get_suffs([{[H|X],N}|Acc], N+1, X);
-get_suffs(Acc,_,[]) -> Acc.
+get_suffs(Acc, N, [H|X],P) ->
+	get_suffs([{[H|X],N,P}|Acc], N+1, X, H);
+get_suffs(Acc,_,[],_) -> Acc.
 
 
 
-%% returns the list of matches of the given Qsec sequence against 
+%% returns the list of matches of the given Qseq sequence against 
 %% the reference sequence represented as FM index
-find_match(Qsec, FM) ->
-	[H|Tail] = lists:reverse(Qsec),
+find_match(Qseq, FM) ->
+	[H|Tail] = lists:reverse(Qseq),
 	%initialize_sp_ep(H,1,size(FM),FM)
 	case find_match(H,1,size(FM),Tail,FM) of
 		no_match -> [];
 		{Sp,Se} -> get_pos([],Sp,Se,FM)
 	end.
 
-find_match(C1, Sp, Ep, [C2|Qsec], FM) ->
-	io:format("Looking for {~p,~p}~n",[C1,C2]),
-	io:format("Range:[~p - ~p]~n",[Sp,Ep]),
+find_match(C1, Sp, Ep, [C2|Qseq], FM) ->
+	%io:format("Looking for {~p,~p}~n",[C1,C2]),
+	%io:format("Range:[~p - ~p]~n",[Sp,Ep]),
 	case se(Sp,not_found,Ep,not_found,C1,C2,FM) of
 		not_found -> no_match;
-		{Sp1,Ep1} -> find_match(C2, Sp1, Ep1, Qsec, FM)
+		{Sp1,Ep1} -> find_match(C2, Sp1, Ep1, Qseq, FM)
 	end;
 
 find_match(_, Sp, Ep, [], _) -> {Sp,Ep}.
@@ -138,7 +177,7 @@ se(Sp,State1,Ep,State2,C1,C2,FM) ->
 			{State22, Ep1} = get_state(Ep,up,C1,C2,FM);
 		found -> State22 = found, Ep1 = Ep
 	end,
-	io:format("(~p,~p), (~p,~p)~n",[Sp1,State11,Ep1,State22]),
+	%io:format("(~p,~p), (~p,~p)~n",[Sp1,State11,Ep1,State22]),
 	
 	case Sp1 > Ep1 of
 		true -> not_found;
@@ -167,3 +206,22 @@ get_pos(Acc,Sp,Ep,FM) ->
 	{_,_,_,SA} = element(Sp,FM),
 	get_pos([SA|Acc],Sp+1,Ep,FM).
 
+
+
+get_subseq(Qseq) -> get_subseq(Qseq, [], 0).
+
+get_subseq(_,Queue,Pos) when length(Queue) == 8 -> 
+	[{{H,_},_}|_] = Queue,
+	{[H|[ X || {{_,X},_} <- Queue]],Pos};
+get_subseq([_],_,_) -> not_found;
+get_subseq([X1,X2|Seq], Queue, Pos) ->
+	%io:format("{~p,~p}, Q: ~p~n",[X1,X2,Queue]),
+	case lists:keyfind({X1,X2},1,Queue) of
+		false -> get_subseq([X2|Seq], [{{X1,X2},1}|Queue], Pos);
+		_ -> 
+			{Queue1,Pos1} = remove(X1,X2, lists:reverse([{{X1,X2},1}|Queue]), 1 ),
+			get_subseq([X2|Seq], Queue1, Pos+Pos1)
+	end.
+
+remove(X1,X2, [{{X1,X2},1}|Ls], N) -> {lists:reverse(Ls),N};
+remove(X1,X2, [_|Ls], N) -> remove(X1,X2, Ls, N+1).
