4ba94aa7d34a12f4a957f80205c75d1d6d999467
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2015-03-20 17:19:01 +0200

    new search function that looks for a previous element in the sequence developed

--- src/bwa.erl
+++ src/bwa.erl
@@ -23,59 +23,48 @@ find_seeds(Subseq,FM) ->
 		$T -> Sp = Pt, Ep = size(FM)
 	end,
 	io:format("Sp:~p, Ep:~p~n",[Sp,Ep]),
-	case find_seeds(H, 1, Sp, Ep, Tail, FM) of
+	case find_seeds(1, Sp, Ep, Tail, FM) of
 		no_match -> [];
 		{Sp1,Ep1,N} -> 
 			get_pos(N,[],Sp1,Ep1,FM,Sp)
 	end.
 
-find_seeds(C1, N, Sp, Ep, [C2|Qseq], FM) ->
+find_seeds(N, Sp, Ep, [C2|Qseq], FM) ->
 	%io:format("Looking for {~p,~p}~n",[C1,C2]),
 	%io:format("Range:[~p - ~p]~n",[Sp,Ep]),
-	case se(Sp,not_found,Ep,not_found,C1,C2,FM) of
+	case se_down(Sp,Ep,C2,FM) of
 		not_found -> {Sp,Ep,N};
-		{Sp1,Ep1} when Ep1-Sp1 > ?RANGE -> find_seeds(C2, N+1, Sp1, Ep1, Qseq, FM);
-		{Sp1,Ep1} -> {Sp1,Ep1,N+1} 
-	end;
-find_seeds(_, N, Sp, Ep, [], _) -> {Sp,Ep,N}.
-
-
-
-se(	Sp,found,Ep,found,_,_,FM) -> 
-	{_,_,Sp1,SA1} = element(Sp,FM),
-	{_,_,Ep1,SA2} = element(Ep,FM),
-	%io:format("SA range: ~p,~p~n",[SA1,SA2]),
-	{Sp1,Ep1};
-se(Sp,State1,Ep,State2,C1,C2,FM) ->
-	case State1 of
-		not_found ->
-			{State11, Sp1} = get_state(Sp,down,C1,C2,FM);
-		found -> State11 = found, Sp1 = Sp
-	end,
-	case State2 of
-		not_found ->
-			{State22, Ep1} = get_state(Ep,up,C1,C2,FM);
-		found -> State22 = found, Ep1 = Ep
-	end,
-	%io:format("(~p,~p), (~p,~p)~n",[Sp1,State11,Ep1,State22]),
-	
-	case Sp1 > Ep1 of
-		true -> not_found;
-		_ -> se(Sp1,State11,Ep1,State22,C1,C2,FM)
-	end. 
-
-
-
-get_state(P,Dir,C1,C2,FM) ->
-	{F,L,_,_} = element(P,FM),
-	case {F,L} of
-		{C1,C2} -> {found,P};
-		_ ->
-			case Dir of
-				down -> {not_found,P+1};
-				up -> {not_found,P-1}
+		Sp1 ->
+			Ep1 = se_up(Ep,C2,FM),
+			case Ep1-Sp1 > ?RANGE of
+				true -> find_seeds(N+1, Sp1, Ep1, Qseq, FM);
+				_ -> {Sp1,Ep1,N+1}
 			end
+	end;
+find_seeds(N, Sp, Ep, [], _) -> {Sp,Ep,N}.
+
+
+se_down(Ep,Ep,C2,FM) -> 
+	{_,L,P,_} = element(Ep,FM),
+	case L == C2 of
+		true -> P;
+		_ -> not_found
+	end;
+se_down(Sp,Ep,C2,FM) -> 
+	{_,L,P,_} = element(Sp,FM),
+	case L == C2 of
+		true -> P;
+		_ -> se_down(Sp+1,Ep,C2,FM)
 	end.
+
+
+se_up(Ep,C2,FM) ->
+	{_,L,P,_} = element(Ep,FM),
+	case L == C2 of
+		true -> P;
+		_ -> se_up(Ep-1,C2,FM)
+	end.
+
 
 
 
