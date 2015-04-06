f10f7a755ce6d71e8b25e0de3e2b97586f4fe2c5
Bohdan Korniyenko <bohdan.korniyenko@gmail.com>
2015-04-06 15:11:56 +0300

    minor refactoring

--- src/master.erl
+++ src/master.erl
@@ -13,26 +13,18 @@ start_link(Args) ->
 register_workers(MasterPid, Pids) ->
   gen_server:call(MasterPid, {register_workers, Pids}).
 
-%% test
-
 test() ->
   lager:start(),
 
   %% Create a master process
   {ok, MPid} = ?MODULE:start_link([]),
   %% Create worker processes
-  {ok, WPid1} = worker_bwt:start_link("GL000192.1"),
-  {ok, WPid2} = worker_bwt:start_link("GL000192.1"),
-  {ok, WPid3} = worker_bwt:start_link("GL000192.1"),
-  {ok, WPid4} = worker_bwt:start_link("GL000192.1"),
-  {ok, WPid5} = worker_bwt:start_link("GL000192.1"),
-  {ok, WPid6} = worker_bwt:start_link("GL000192.1"),
+  WorkersNum = 1,
+  Pids = lists:map(fun(_) -> {ok, WPid} = worker_bwt:start_link(), WPid end, lists:seq(1, WorkersNum)),
   %% Associate them with the master
-  ok = master:register_workers(MPid, [WPid1, WPid2, WPid3, WPid4, WPid5, WPid6]),
+  ok = master:register_workers(MPid, Pids),
   %% Tell the master to run
   SeqFileName = "bwt_files/SRR770176_1.fastq",
-%%   Size = fastq:fold(fun(_,A) -> A+1 end, 0, SeqFileName),
-%%   lager:info("FasqQ size ~p", [Size]),
   ok = gen_server:call(MPid, {run, SeqFileName}, infinity).
 
 %% gen_server callbacks
@@ -46,7 +38,7 @@ init(_Args) ->
 terminate(Reason, State) ->
   lager:info("Master terminated: ~p", [{Reason, State}]).
 
-%% It's ok
+%% A worker is finished
 handle_info({'DOWN',_Ref,process,_Pid,normal}, State) ->
   {noreply, State}.
 
@@ -85,20 +77,12 @@ assign([Pid|Workers], Dev) ->
   N = 10000,
   case fastq:read_seq(Dev, N) of
     {ok, SeqList} when is_list(SeqList) ->
-     % FmIndex = {fmindex, {chromosome, "GL000192.1"}},
+      FmIndex = {fmindex, {chromosome, "GL000192.1"}},
       SeqList1 = lists:map(fun({_,Seq}) -> Seq end, SeqList),
-      Workload = {fastq, SeqList1},
+      Workload = {FmIndex, {fastq, SeqList1}},
       ok = worker_bwt:execute(Pid, Workload, self()),
       assign(Workers, Dev);
     eof ->
       io:format("End of fastq file~n"),
       [Pid|Workers]
   end.
-
-split(Number, ChunksNum) ->
-  split_inner(lists:seq(1,Number,Number div ChunksNum), []).
-
-split_inner([From, To], Acc) -> 
-  [{From, To}|Acc];
-split_inner([From | [To | _] = Rest], Acc) ->
-  split_inner(Rest, [{From, To}|Acc]).
--- src/worker_bwt.erl
+++ src/worker_bwt.erl
@@ -1,48 +1,49 @@
 -module(worker_bwt).
 -behaviour(gen_server).
 
--export([start_link/1, execute/3]).
+-export([start_link/0, execute/3]).
 -export([init/1, handle_info/2, handle_call/3, handle_cast/2]).
 
 -include("bwt.hrl").
 
 %% api
-start_link(Args) ->
-  gen_server:start_link(?MODULE, Args, []).
+start_link() ->
+  gen_server:start_link(?MODULE, {}, []).
 
 execute(Pid, Workload, MasterPid) ->
   gen_server:call(Pid, {execute, Workload, MasterPid}, infinity).
 
 %% callbacks
 
--record(state, {busy=false, master, slave, fm, workload}).
+-record(state, {busy=false, master, slave, fms=[], workload}).
 
-init(ChromoName) ->
-  FM = bwt:get_index(ChromoName),
+init(_) ->
   lager:info("Worker have read the FM-index"),
-  {ok, #state{fm=FM}}.
+  {ok, #state{}}.
 
-%% It's ok
+%% The slave is finished
 handle_info({'DOWN',Ref,process,_Pid,normal}, S=#state{slave=Ref}) ->
   {noreply, S}.
 
-handle_call({execute, Workload, MasterPid}, _From, S=#state{busy=false, fm=FM}) ->
-  %{{fmindex, {chromosome, ChromoName}}, {fastq, QseqList}} = Workload,
-  {fastq, QseqList} = Workload,
+handle_call({execute, Workload, MasterPid}, _From, S=#state{busy=false, fms=FMs}) ->
+  {{fmindex, {chromosome, ChromoName}}, {fastq, QseqList}} = Workload,
+  {FM, FMs1} =
+    case proplists:get_value(ChromoName, FMs) of
+      undefined ->
+        FM = bwt:get_index(ChromoName),
+        {FM, [{ChromoName, FM}]};
+      FM ->
+        {FM, FMs}
+    end,
   lager:info("Worker ~p got workload ~p", [self(), length(QseqList)]),
   SlaveRef = spawn_slave(FM, QseqList),
-  {reply, ok, S#state{busy=true, master=MasterPid, slave=SlaveRef}}.
+  {reply, ok, S#state{busy = true, master = MasterPid, slave = SlaveRef, fms = FMs1}}.
 
-handle_cast({results, _Results} = Msg, S = #state{busy=true, master=MasterPid, workload = Workload, fm=FM}) ->
+%% a cast from the slave
+handle_cast({results, _Results} = Msg, S = #state{busy=true, master=MasterPid}) ->
+  %% retranslate the results to the master
   ok = gen_server:call(MasterPid, Msg, infinity),
-  S1 = case Workload of
-    undefined ->
-      S#state{busy=false};
-    {fastq, QseqList} ->
-      SlaveRef = spawn_slave(FM, QseqList),
-      S#state{busy=true, slave=SlaveRef}
-  end,
-  {noreply, S1}.
+  {noreply, S#state{busy=false}}.
 
 %% private
 
