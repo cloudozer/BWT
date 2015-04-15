97844c63afcd039963b8c7292a04905667d14573
Bohdan Korniyenko <bohdan.korniyenko@gmail.com>
2015-04-15 16:42:19 +0300

    disable worker_bwt

--- src/bwt_sup.erl
+++ src/bwt_sup.erl
@@ -24,6 +24,6 @@ start_link() ->
 
 init([]) ->
     {ok, { {one_for_one, 5, 10}, [
-      {worker_bwt, {worker_bwt, start_link, [{master, 'master@erlangonxen.org'}]}, permanent, 5000, worker, [worker_bwt]}
+%      {worker_bwt, {worker_bwt, start_link, [{master, 'master@erlangonxen.org'}]}, permanent, 5000, worker, [worker_bwt]}
     ]} }.
 
