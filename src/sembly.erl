df9dd49e4371318d8236da538ee2ab4a230f02e1
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2015-05-21 14:07:39 -0700

    ip

--- src/sembly.erl
+++ src/sembly.erl
@@ -7,6 +7,10 @@
 -module(sembly).
 -export([t/0]).
 
+-define(WORKERS_NBR,2).
+-define(X_TRIALS,1). 	%% number of times a read will be tried to attach to the graph
+
+
 
 t() ->
 	Chromo = "GL000207.1",
@@ -15,33 +19,68 @@ t() ->
 t(Chromo, Len) ->
 	Coverage = 7,
 	{ok,Ref} = file:read_file("bwt_files/"++Chromo++".ref"),
-	Size = size(Ref),
+	D = round(2*Len/Coverage),
 	Entire_seq = binary_to_list(Ref),
-	Reads_nbr = round(Size/Len*Coverage),
-	Sembly = assemble([ {lists:sublist(Entire_seq,J,100),J} || 
-		J <-[ random:uniform(Size-Len+1) || _ <- lists:seq(1,Reads_nbr)]]),
-	io:format("~p~n",[Sembly]).
+	
+	Reads = get_reads(Entire_seq,D,Len,0,[]),
+	assemble(Reads),
+
+	io:format("~p~n",[Reads]).
 
 
-assemble(Reads) ->
-	Q_reads = encode_G(Reads),
-	gc_graph:build_graph(Q_reads).
+get_reads(Entire_seq,D,Len,_,Acc) when length(Entire_seq) =< Len+D -> 
+	[lists:nthtail(length(Entire_seq)-Len,Entire_seq)|Acc];
+get_reads(Entire_seq,D,Len,J,Acc) ->
+	D1 = random:uniform(D),
+	get_reads(lists:nthtail(D1,Entire_seq),D,Len,J+D1,[{lists:sublist(Entire_seq,D1+1,Len),J+D1}|Acc]).
+
+
+assemble([R|Reads]) ->
+	Pid = spawn(assembler,start_worker,[self(),R]),
+	build_graph(queue:from_list(Reads),[Pid],length(Reads)*?X_TRIALS).
+	
+
+build_graph(Q,Workers,0) ->
+	io:format("~p reads were not matched~n",[queue:len(Q)]),
+	[ Pid ! {get_graph} || Pid <- Workers ],
+	assemble_graph(length(Workers),[]);
+
+build_graph(Q,Workers,N) ->
+	case queue:out(Q) of
+		{{value,Read},Q1} -> 
+			build_graph(Q1,Workers,Read,N);
+		{empty,_} -> 
+			[ Pid ! {get_graph} || Pid <- Workers ],
+			assemble_graph(length(Workers),[])
+	end.
+	
+build_graph(Q,Workers,Read,N) -> 
+	%% sends Read to all workers which then try to attach it to their sub-graphs
+	lists:foreach(fun(Wpid) -> Wpid ! Read end, Workers),
+	Res = [ receive Msg -> Msg end || _ <- Workers ],
+	%io:format("Got messages from ~p workers: ~p~n",[length(Res),Res]),
+	
+	%% if Read attached at more than one subgraphs -> merge these sub_graphs
+	%% if Read attached at no subgraphs -> spawn a new worker or put the Read back in the queue
+	L = length(lists:filter(fun(not_attached)->false; (_)->true end,Res)),
+	case L of 
+		0 -> 	%% spawn a new assembler or put Read back to the queue
+			Q1 = queue:in(Read,Q),
+			build_graph(Q1,Workers,N-1);
+		1 -> 
+			io:format("read attached to the only subgraph~n"),
+			build_graph(Q,Workers,N);
+		2 -> 
+			io:format("Merge subgraphs~n"),
+			build_graph(Q,Workers,N)
+	end.
+	
 
 
 
-
-encode_G(Reads) ->
-	encode_G(Reads,[]).
-
-encode_G([{Read,J}|Reads],Acc) -> encode_G(Reads,[{Read,J,extract_G(Read)}|Acc]);
-encode_G([],Acc) -> queue:from_list(Acc).
-
-
-extract_G(Read) -> extract_G(Read,1,[]).
-
-extract_G([$G|Read],Count,Acc) -> extract_G(Read,1,[Count|Acc]);
-extract_G([_|Read],Count,Acc) -> extract_G(Read,Count+1,Acc);
-extract_G([],Count,Acc) -> lists:reverse([Count|Acc]).
+%% collect all sub-graphs
+assemble_graph(0,Acc) -> Acc;
+assemble_graph(Workers_nbr,Acc) -> receive Graph -> assemble_graph(Workers_nbr-1, [Graph|Acc]) end.
 
 
 
