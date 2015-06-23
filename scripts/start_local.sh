e6ca2a7165be9232387d50df58d1a273791a4b92
Bohdan <bohdan.korniyenko@cloudozer.com>
2015-06-23 14:07:37 +0200

    use schedule:chunks_to_box; change start_local.sh

--- scripts/start_local.sh
+++ scripts/start_local.sh
@@ -5,12 +5,14 @@
 main([]) ->
   io:format("Usage: start_local.sh SeqFileName Chromosome Debug?~n");
 
-main([SeqFileName, Chromosome]) ->
-  main([SeqFileName, Chromosome, "false"]);
+main([SeqFileName, ChromosomeList]) ->
+  main([SeqFileName, ChromosomeList, "false"]);
 
-main([SeqFileName, Chromosome, DebugStr]) ->
+main([SeqFileName, ChromosomeListRaw, DebugStr]) ->
+  ChromosomeList = lists:foldl(fun(<<>>,A)->A; (B,A)->[binary_to_list(B)|A] end, [], 
+    binary:split(list_to_binary(ChromosomeListRaw), [<<",">>, <<"[">>, <<"]">>], [global])), 
   Debug = list_to_atom(DebugStr),
-  source:test(SeqFileName, Chromosome, Debug),
+  source:test(SeqFileName, ChromosomeList, Debug),
   receive
     {stop, Secs} ->
       io:format("It's all over. ~.1f sec.~n", [Secs])
