56903bcca06412f3a0212c3e4880ca242e894bca
Maxim Kharchenko <maxim.kharchenko@gmail.com>
2015-07-16 07:27:11 +0300

    navel uses active socket

--- src/navel.erl
+++ src/navel.erl
@@ -3,8 +3,10 @@
 -define(SERVER, ?MODULE).
 
 -define(PROXY_TCP_PORT, 9223).
--define(SOCK_OPTS, [{active,false},{reuseaddr,true},binary]).
+-define(SOCK_OPTS, [{active,true},{reuseaddr,true},binary]).
 -define(CHUNK_SIZE, 1440).
+
+-define(RESTART_AFTER, 1000).
 
 -record(nv, {node,hood =[]}).
 
@@ -44,13 +46,21 @@ start0(Node) -> start(Node).	%%OBSOLETE
 connect(Addr) ->
 	{ok,S} = gen_tcp:connect(Addr, ?PROXY_TCP_PORT, ?SOCK_OPTS),
 	Postman = spawn(fun() -> nice_postman(S) end),
-	door_bell(Postman).
+	ok = gen_tcp:controlling_process(S, Postman),
+	door_bell(Postman, true).
 
-door_bell(Postman) ->
-	gen_server:call(?SERVER, {door_bell,Postman}).
+door_bell(Postman) -> door_bell(Postman, false).
+door_bell(Postman, Intro) ->
+	gen_server:call(?SERVER, {door_bell,Postman,Intro}).
 
 introduce(Homie, Name) ->
 	gen_server:call(?SERVER, {introduce,Homie,Name}).
+
+substitute(A, B) ->
+	gen_server:call(?SERVER, {substitute,A,B}).
+
+signoff(Homie) ->
+	gen_server:call(?SERVER, {signoff,Homie}).
 
 get_node() -> gen_server:call(?SERVER, get_node).
 
@@ -83,13 +93,29 @@ init([Node,PortInc]) ->
 handle_call(get_node, _From, #nv{node =MyNode} =St) ->
 	{reply,MyNode,St};
 
-handle_call({door_bell,Homie}, _From, #nv{hood =Hood} =St) ->
+handle_call({door_bell,Homie,false}, _From, #nv{hood =Hood} =St) ->
 	true = link(Homie),
     {reply,ok,St#nv{hood =[{noname,Homie}|Hood]}};
 
+handle_call({door_bell,Homie,true}, From, #nv{hood =Hood} =St) ->
+	true = link(Homie),
+    {noreply,St#nv{hood =[{noname,Homie,From}|Hood]}};
+
 handle_call({introduce,Homie,Name}, _From, #nv{hood =Hood} =St) ->
-	{value,{noname,_},Hood1} = lists:keytake(Homie, 2, Hood),
+	case lists:keytake(Homie, 2, Hood) of
+		{value,{noname,_},Hood1} -> Hood1;
+		{value,{noname,_,Notify},Hood1} -> gen_server:reply(Notify, ok), Hood1 end,
 	{reply,ok,St#nv{hood =[{Name,Homie}|Hood1]}};
+
+handle_call({substitute,A,B}, _From, #nv{hood =Hood} =St) ->
+	unlink(A), link(B),
+	{value,{Name,_},Hood1} = lists:keytake(A, 2, Hood),
+	{reply,ok,St#nv{hood =[{Name,B}|Hood1]}};
+
+handle_call({signoff,Homie}, _From, #nv{hood =Hood} =St) ->
+	{value,{Name,_},Hood1} = lists:keytake(Homie, 2, Hood),
+	io:format("navel: connection to ~s lost\n", [Name]),
+	{reply,ok,St#nv{hood =Hood1}};
 
 handle_call({expose,Name}, _From, #nv{hood =Hood} =St) ->
 	case lists:keyfind(Name, 1, Hood) of
@@ -116,6 +142,7 @@ code_change(_OldVsn, State, _Extra) ->
 acceptor(L) ->
 	{ok,S} = gen_tcp:accept(L),
 	Postman = spawn(fun() -> nice_postman(S) end),
+	ok = gen_tcp:controlling_process(S, Postman),
 	door_bell(Postman),
 	acceptor(L).
 
@@ -124,38 +151,76 @@ nice_postman(S) ->
 	send_message(S, {'$iam',MyNode}),
 	postman(S).
 
-postman(S) ->
-	case receive_message(S, 0) of
-		{ok,{'$iam',RemoteName}} ->
-			{ok,{{A,B,C,D},P}} = inet:peername(S),
-			io:format("navel: connected to ~w at ~w.~w.~w.~w:~w\n", [RemoteName,A,B,C,D,P]),
-			introduce(self(), RemoteName),
-			postman(S);
-		{ok,{'$call',M,F,As}} ->
-			Returns = (catch apply(M, F, As)),
-			send_message(S, {'$returns',Returns}),
-			postman(S);
-		{ok,{'$call_no_return',M,F,As}} ->
-			case (catch apply(M, F, As)) of
-				{'EXIT',Reason} ->
-					io:format("navel: exception caught while calling ~w:~w/~w: ~p\n",
-							[M,F,length(As),Reason]);
-				_ -> ok end,
-			postman(S);
-		undefined -> sort_mail(S) end.
+postman(S) -> postman(S, 0, 0, [], nocall, 0).
 
-sort_mail(S) ->
+rookie_postman(S, ExpSz, IncSz, Chips, Call) ->
+	receive take_over -> ok end,
+	postman(S, ExpSz, IncSz, Chips, Call, 0).
+
+forward_mail(Addressee) ->
+	receive X -> Addressee ! X, forward_mail(Addressee)
+			after 0 -> done end.
+
+postman(S, ExpSz, IncSz, Chips, Call, N) when N >= ?RESTART_AFTER ->
+	Rookie = spawn(fun() -> rookie_postman(S, ExpSz, IncSz, Chips, Call) end),
+	substitute(self(), Rookie),
+	ok = gen_tcp:controlling_process(S, Rookie),
+	forward_mail(Rookie),
+	%%io:format("navel: restarted ~w as ~w\n", [self(),Rookie]),
+	Rookie ! take_over;
+
+postman(S, ExpSz, IncSz, Chips, Call, N) ->
 	receive
 		{From,{call,M,F,As}} ->
+			true = Call =:= nocall,
 			ok = send_message(S, {'$call',M,F,As}),
-			{ok,{'$returns',Returns}} = receive_message(S),
-			From ! {returns,Returns},
-			sort_mail(S);
+			postman(S, ExpSz, IncSz, Chips, From, N+1);
 		{_From,{call_no_return,M,F,As}} ->
 			ok = send_message(S, {'$call_no_return',M,F,As}),
-			sort_mail(S)
-		after 0 -> postman(S) end.
+			postman(S, ExpSz, IncSz, Chips, Call, N+1);
+		{tcp,S,Chip} ->
+			{ExpSz1,IncSz1,Chips1,Call1} = incoming(S, Chip, ExpSz, IncSz, Chips, Call),
+			postman(S, ExpSz1, IncSz1, Chips1, Call1, N+1);
+		{tcp_closed,S} -> signoff(self()) end.
 
+incoming(S, Chip, 0, _IncSz, Chips, Call) ->
+	case merge(Chip, Chips) of
+		<<ExpSz:32,Data/binary>> -> incoming(S, Data, ExpSz, 0, [], Call);
+		Bin -> {0,byte_size(Bin),[Bin],Call} end;
+
+incoming(S, Chip, ExpSz, IncSz, Chips, Call) when IncSz+byte_size(Chip) >= ExpSz ->
+	<<MsgBin:ExpSz/binary,More/binary>> = merge(Chip, Chips),
+	<<PadSz:16,_:PadSz/binary,Payload/binary>> = MsgBin,
+	Call1 = action(S, binary_to_term(Payload), Call),
+	incoming(S, More, 0, 0, [], Call1);
+
+incoming(_S, Chip, ExpSz, IncSz, Chips, Call) ->
+	{ExpSz,IncSz+byte_size(Chip),[Chip|Chips],Call}.
+
+merge(Chip, Chips) -> list_to_binary(lists:reverse([Chip|Chips])).
+
+action(S, {'$iam',RemoteName}, Call) ->
+	{ok,{{A,B,C,D},P}} = inet:peername(S),
+	io:format("navel: connected to ~s at ~w.~w.~w.~w:~w \n", [RemoteName,A,B,C,D,P]),
+	introduce(self(), RemoteName),
+	Call;
+
+action(S, {'$call',M,F,As}, Call) ->
+	Returns = (catch apply(M, F, As)),
+	send_message(S, {'$returns',Returns}),
+	Call;
+
+action(_S, {'$call_no_return',M,F,As}, Call) ->
+	case (catch apply(M, F, As)) of
+		{'EXIT',Reason} ->
+			io:format("navel: exception caught while calling ~w:~w/~w: ~p\n",
+					[M,F,length(As),Reason]), Call;
+		_ -> Call end;
+
+action(_S, {'$returns',Returns}, Call) ->
+	true = Call =/= nocall,
+	Call ! {returns,Returns},
+	nocall.
 
 %% Message encoding:
 %%
@@ -167,24 +232,6 @@ sort_mail(S) ->
 %%
 %% The purpose of the padding is to align the message to CHUNK_SIZE
 %%
-
-receive_message(Sock) -> receive_message(Sock, infinity).
-receive_message(Sock, Timeout) ->
-	case gen_tcp:recv(Sock, 4, Timeout) of
-		{ok,<<MsgSz:32>>} ->
-			{ok,Chunks} = chunks(Sock, MsgSz),
-			<<PadSz:16,_Padding:PadSz/binary,Payload/binary>> = list_to_binary(Chunks),
-			Msg = binary_to_term(Payload),
-			%%io:format("navel: receive ~P\n", [Msg,12]),
-			{ok,Msg};
-		{error,timeout} -> undefined end.
-
-chunks(Sock, Sz) -> chunks(Sock, Sz, []).
-chunks(_Sock, 0, Acc) -> {ok,lists:reverse(Acc)};
-chunks(Sock, Sz, Acc) ->
-	Sz1 = if Sz > ?CHUNK_SIZE -> ?CHUNK_SIZE; true -> Sz end,
-	{ok,Bin} = gen_tcp:recv(Sock, Sz1),
-	chunks(Sock, Sz - Sz1, [Bin|Acc]).
 
 send_message(Sock, Term) ->
 	%%io:format("navel: send ~P\n", [Term,12]),
