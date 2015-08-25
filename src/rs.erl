467242f5c708ece4d6be54af33fc62c82f1ef412
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2015-08-25 14:11:38 +0300

    old fun that started cluster deleted

--- src/rs.erl
+++ src/rs.erl
@@ -23,50 +23,13 @@ start(SeqFileName, HttpStorage) ->
 			{run, Alqs,SFs, SinkRef} ->
 StartTime = now(),
 				r_source(Reads,Alqs,SFs,length(SFs),SinkRef),
-io:format("Fastq complited within ~p secs.", [timer:now_diff(now(), StartTime) / 1000000])
+io:format("Fastq completed within ~p secs.", [timer:now_diff(now(), StartTime) / 1000000])
 		end
 	end),
 	Ref = {navel:get_node(), Pid},
 	{ok, Ref}.
 
-start_cluster(Boxes,ChromoLs,SeqFileName,HttpStorage,LingdRef) ->
-  % TODO: refactor it
-  % box RAM in Gb
-  Box_mem = 24,
 
-  %% TODO: add availability test
-  IndexUrl = HttpStorage ++ "/fm_indices/index.json",
-  {ok, LsJson} = http:get(IndexUrl),
-  FilesList = jsx:decode(LsJson),
-	case schedule:chunks_to_box(ChromoLs,FilesList,length(Boxes),Box_mem) of
-		not_enough_memory ->
-			io:format("Not enough memory~n"),
-      throw(no_memory);
-
-		Schedule ->
-			Schedule1 = lists:zip(Boxes,Schedule),
-			io:format("Schedule: ~p~n",[Schedule1]),
-
-      Self = {navel:get_node(),self()},
-
-      %% Start sink app
-      {ok,SinkHost} = lingd:create(LingdRef, sink, [{memory,2024}]),
-      {_Box,Sink,Schedule2} = navel:call(sink, sk, start_sink, [Schedule1,Self]),
-      io:format("Sink started. Sink pid: ~p~nSchedule:~p~n",[Sink,Schedule2]),
-
-      %% Create just one alq
-      Schedule3 = alq:start_alq(Schedule2,SinkHost,Sink,LingdRef), % {Box_id,Alq,Chunk_files}
-      io:format("Alq started. New Schedule: ~p~n",[Schedule3]),
-
-      {Alqs, SFs} = sf:start_SF(Schedule3,LingdRef,HttpStorage,Self),
-      io:format("SFs started. Alqs & SFs: ~p~n~p~n",[Alqs,SFs]),
-
-      SeqFileNameUrl = HttpStorage ++ "/" ++ SeqFileName,
-      {ok,Reads} = http:get(SeqFileNameUrl),
-StartTime = now(),
-			r_source(Reads,Alqs,SFs,length(SFs),Sink),
-io:format("Fastq complited within ~p secs.", [timer:now_diff(now(), StartTime) / 1000000])
-	end.
 
 produce_workload(N, Fastq) ->
   produce_workload(N, Fastq, []).
