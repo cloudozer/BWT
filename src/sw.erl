d6febc5f0e764826ffc2dc0727ff823ec5878927
Bohdan Korniyenko <bohdan.korniyenko@gmail.com>
2015-09-04 12:36:38 +0300

    fix simple_match

--- src/sw.erl
+++ src/sw.erl
@@ -166,7 +166,8 @@ get_str(N,Dir) ->
 
 
 simple_match(Ls1,Ls2) when length(Ls1) =:= length(Ls2) -> 
-	simple_match(Ls1,Ls2,?MATCH*length(Ls1),?MATCH*length(Ls1)*0.9).
+	Val = simple_match(Ls1,Ls2,?MATCH*length(Ls1),?MATCH*length(Ls1)*0.9),
+	{Val, integer_to_list(Ls1) ++ "M"}.
 
 simple_match(_,_,Val,Thr) when Val < Thr -> no_match;
 simple_match([$N|Ls1],[_|Ls2],Val,Thr) -> simple_match(Ls1,Ls2,Val-?MATCH+?UNKNOWN,Thr);
