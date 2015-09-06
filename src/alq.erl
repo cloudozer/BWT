047171a56717e099bd7e4a7c3259a83a9b643a32
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2015-09-06 13:33:02 +0300

    Seeds are sent in the batches

--- src/alq.erl
+++ src/alq.erl
@@ -41,37 +41,34 @@ cm_balancer(?CIGAR_MAKER_NBR,Sink,[],[Pid={CmN,CmP}|CMs]) ->
 	receive
 		quit -> terminate_cm(?CIGAR_MAKER_NBR);
 
-		{Read_name,Chromo,Read,[{Pos,Ref}]} ->
-			navel:call_no_return(CmN,erlang,send,[CmP,{Ref,Read,Read_name,Chromo,Pos}]),
-			cm_balancer(?CIGAR_MAKER_NBR,Sink,[],CMs);
-
-		{Read_name,Chromo,Read,[{Pos,Ref}|Ref_seeds]} ->
-			navel:call_no_return(CmN,erlang,send,[CmP,{Ref,Read,Read_name,Chromo,Pos}]),
-			cm_balancer(?CIGAR_MAKER_NBR,Sink,[{Read_name,Chromo,Read,Ref_seeds}],CMs);
-
 		{Pid1,ready} -> cm_balancer(?CIGAR_MAKER_NBR,Sink,[],[Pid1,Pid|CMs]);
 
 		fastq_done when length([Pid|CMs]) =:= ?CIGAR_MAKER_NBR ->
 			{SN,SP} = Sink,
 			navel:call_no_return(SN,erlang,send,[SP,fastq_done]),
-			io:format("Alq confirmed that fastq_done~n")
+			io:format("Alq confirmed that fastq_done~n");
+
+		NewTasks ->
+			cm_balancer(?CIGAR_MAKER_NBR,Sink,NewTasks,[{CmN,CmP}|CMs])
+
 	end;
 
 cm_balancer(?CIGAR_MAKER_NBR,Sink,[{_Read_name,_Chromo,_Read,[]}|Tasks],CMs) ->
 	cm_balancer(?CIGAR_MAKER_NBR,Sink,Tasks,CMs);
 
-cm_balancer(?CIGAR_MAKER_NBR,Sink,[{Read_name,Chromo,Read,[{Pos,Ref}|Ref_seeds]}|Tasks],[]) ->
+cm_balancer(?CIGAR_MAKER_NBR,Sink,[{Read_name,Chromo,Read,[{Pos,Ref}|Ref_seeds]}|Tasks]=OldTasks,[]) ->
 	receive
 		quit -> terminate_cm(?CIGAR_MAKER_NBR);
-
-		{_Read_name,_Chromo,_Read,_Ref_seeds}=Task -> cm_balancer(?CIGAR_MAKER_NBR,Sink,[Task|Tasks],[]);
 
 		{{CmN,CmP},ready} ->
 			navel:call_no_return(CmN,erlang,send,[CmP,{Ref,Read,Read_name,Chromo,Pos}]),
 			case Ref_seeds of
 				[] -> cm_balancer(?CIGAR_MAKER_NBR,Sink,Tasks,[]);
 				_ -> cm_balancer(?CIGAR_MAKER_NBR,Sink,[{Read_name,Chromo,Read,Ref_seeds}|Tasks],[])
-			end
+			end;
+
+		NewTasks -> cm_balancer(?CIGAR_MAKER_NBR,Sink,OldTasks++NewTasks,[])
+
 	after 10000 ->
 		throw({timeout, cm_balancer, tasks_orevflow})
 	end;
